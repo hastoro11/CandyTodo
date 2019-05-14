c843e69eff3d7bafcd78e3153ea3a9a9e7cda029
Gabor Sornyei <info@sornyei.com>
2019-05-14 07:46:04 +0200

    Initial Commit

---
+++ CandyTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CandyTodo
+//
+//  Created by Gabor Sornyei on 2019. 05. 14..
+//  Copyright © 2019. Gabor Sornyei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
