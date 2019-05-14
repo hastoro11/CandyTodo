90eec6459b87928e7e0928577f1ac66d1fadc733
Gabor Sornyei <info@sornyei.com>
2019-05-14 08:30:50 +0200

    LaunchScreen with logo

---
+++ CandyTodo/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  CandyTodo
+//
+//  Created by Gabor Sornyei on 2019. 05. 14..
+//  Copyright © 2019. Gabor Sornyei. All rights reserved.
+//
+
+import UIKit
+
+// Colors
+let kBASEPINK_COLOR = UIColor(red: 255/255, green: 238/255, blue: 238/255, alpha: 1)
+let kBASEBLUE_COLOR = UIColor(red: 36/255, green: 59/255, blue: 107/255, alpha: 1)
